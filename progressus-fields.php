32247a1560c8a17b33c29568fd2b9e7b1477573c
Ridwan Arifandi <orangerdigiart@gmail.com>
2019-07-17 17:55:30 +0700

    init files

---
+++ progressus-fields.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Progressus - WooCommerce Fields
+ * Plugin URI:        https://github.com/orangerdev/
+ * Description:       Add apartment number field to billing fields
+ * Version:           1.0.0
+ * Author:            Ridwan Arifandi
+ * Author URI:        https://github.com/orangrdev
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       progressus
+ * Domain Path:       /languages
+ *
+ * WC requires at least: 3.0.0
+ * WC tested up to: 3.5.5
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
